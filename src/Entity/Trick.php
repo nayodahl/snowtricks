4a7def5ce8cb569640bc4eee103adae826c6f7ae
nayodahl <anthony.fachaux@gmail.com>
2020-08-29 18:16:27 +0200

    demo data added with doctrine fixtures

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -6,10 +6,12 @@ use App\Repository\TrickRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=TrickRepository::class)
+ * @UniqueEntity("title")
  */
 class Trick
 {
